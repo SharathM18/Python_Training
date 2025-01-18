caab1d3074d384ec5ba192075538b9a69716cd4e
Sharath M <sharathm5684@gmail.com>
2025-01-18 23:58:50 +0530

    solved scenario questions

---
+++ Scenario_Question/furniture_center.py
@@ -0,0 +1,8 @@
+database = {
+    'pdcc01': {'name': 'chair', 'price': 1500, 'location': 'B1R1C2', 'quantity': 2},
+    'pdcs11': {'name': 'sofa',  'price': 50000, 'location': 'B2R4C1', 'quantity': 'home'},
+    'pdct25': {'name': 'table',  'price': 15000, 'location': 'B3R3C2', 'quantity': 'home'},
+    'pdcd011': {'name': 'desk',  'price': 900, 'location': 'B4R2C3', 'quantity': 13},
+    'pdcR012': {'name': 'shelf',  'price': 2500, 'location': 'B5R1C4', 'quantity': 0}
+}
+
